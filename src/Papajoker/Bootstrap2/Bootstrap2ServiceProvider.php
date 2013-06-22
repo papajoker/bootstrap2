9a38296d86d0449560a1070f86ba5addc0de4165
papajoker <patrickc538@gmail.com>
2013-06-22 20:59:41 +0200

    version 0.1

---
+++ src/Papajoker/Bootstrap2/Bootstrap2ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Papajoker\Bootstrap2;
+
+use Illuminate\Support\ServiceProvider;
+
+class Bootstrap2ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
